f1c30dc6ce385914f58ffe0a72e4347bae4c3515
soulcm <liucm666@outlook.com>
2017-01-12 11:07:10 +0800

    init

---
+++ src/apps/components/main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <h1>
+                hello webpack dll
+            </h1>
+        );
+    }
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './apps/components/main.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
